3034d8fe8458ffcab6a2db34e2c9d8e4b8f0e3fe
maslovsa <s.maslov@corpwebgames.com>
2014-08-25 11:32:01 +0400

    first commit

--- imageFiltering/ViewController.m
+++ imageFiltering/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
 
@@ -26,4 +26,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)sliderValueChanged:(id)sender {
+}
+
+- (IBAction)loadImage:(id)sender {
+}
+
+- (IBAction)saveImage:(id)sender {
+    NSString *message = [NSString stringWithFormat:
+                         @"The value of the slider is: %d", lroundf( self.slider.value) ];
+    
+    UIAlertView *alertView = [[UIAlertView alloc]
+                              initWithTitle:@"Hello, World!"
+                              message:message
+                              delegate:nil
+                              cancelButtonTitle:@"OK"
+                              otherButtonTitles:nil];
+    
+    [alertView show];
+}
 @end
